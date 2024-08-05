29ae6977da8ca107b66f62f351a6130ba152e178
Muhammad Abdullah <mabdullah@ogoul.com>
2024-08-05 11:48:11 +0500

    pages created header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
